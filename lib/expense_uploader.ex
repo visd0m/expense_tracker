6ecc7061e38c771f5f789bf7737e950717e78428
Domenico Visconti <domenico.visconti@prima.it>
2019-11-23 10:56:41 +0100

    upgrade goth library

--- lib/expense_uploader.ex
+++ lib/expense_uploader.ex
@@ -1,4 +1,6 @@
 defmodule ExpenseUploader do
+  require Logger
+
   @spec upload_expenses([Expense.t()], String.t()) ::
           {:error, Tesla.Env.t()} | {:ok, GoogleApi.Sheets.V4.Model.AppendValuesResponse.t()}
   def upload_expenses(expenses, spreadsheet_id) do
@@ -19,9 +21,10 @@ defmodule ExpenseUploader do
 
     connection =
       GoogleApi.Sheets.V4.Connection.new(fn scopes ->
-        Goth.Token.for_scope(Enum.join(scopes, " "))
-        |> elem(1)
-        |> Map.get(:token)
+        case get_token(scopes) do
+          {:ok, token} -> token
+          err -> raise "can not obtain access token, #{inspect(err)}"
+        end
       end)
 
     GoogleApi.Sheets.V4.Api.Spreadsheets.sheets_spreadsheets_values_append(
@@ -34,5 +37,23 @@ defmodule ExpenseUploader do
         values: expenses
       }
     )
+  end
+
+  @spec get_token([String.t()]) :: {:ok, String.t()} | {:error, any}
+  def get_token(scopes) do
+    Logger.info("getting token for scopes=#{scopes}")
+
+    with scopes <- Enum.join(scopes, " "),
+         {:ok, result} <- Goth.Token.for_scope(scopes),
+         {:ok, token} <- Map.fetch(result, :token) do
+      {:ok, token}
+    else
+      {:error, reason} = err ->
+        Logger.error(inspect(reason))
+        err
+
+      err ->
+        {:error, err}
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule ExpenseTracker.MixProject do
       {:csv, "~> 2.3"},
       {:xlsxir, "~> 1.6"},
       {:google_api_sheets, "~> 0.7.0"},
-      {:goth, "~> 1.1"}
+      {:goth, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
